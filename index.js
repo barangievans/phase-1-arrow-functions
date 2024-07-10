d82a4185274a2ccc64bc332c19fdb66da5ff91d5
barangievans <barangievans@gmail.com>
2024-07-10 21:38:57 +0300

    arrow-functions

---
+++ index.js
@@ -0,0 +1,20 @@
+// Function expression for divide
+const divide = function(a, b) {
+    return a / b;
+}
+
+// Arrow function for square
+const square = (x) => {
+    return x * x;
+}
+
+// Arrow function for add
+const add = (a, b) => {
+    return a + b;
+}
+
+module.exports = {
+    divide,
+    square,
+    add
+};
